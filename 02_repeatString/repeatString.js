4b3056df6d5ed1ff654048b0967c13989bcfcfb3
Victor <victorv1996@yahoo.com>
2022-09-20 01:14:30 -0700

    finished code for exercises 1 & 2 and passed all test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word="hey",times=0) {
+    if(times === 0){
+        return "";
+    }else if(times < 0){
+        return "ERROR";
+    }
+    return word.repeat(times);
 };
 
 // Do not edit below this line
